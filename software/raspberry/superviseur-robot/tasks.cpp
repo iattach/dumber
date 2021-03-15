48e523f299c204b34f77a5add473cc49a19ff582
Yunan HOU <yunan8@outlook.com>
2021-03-15 10:09:38 +0100

    prepared merge

--- software/raspberry/superviseur-robot/tasks.cpp
+++ software/raspberry/superviseur-robot/tasks.cpp
@@ -496,7 +496,7 @@ void Tasks::BatteryTask(void *arg) {
 
     while (1) {
         rt_task_wait_period(NULL);
-        cout << "Level battery update";
+        cout <<endl << "Level battery update";
         rt_mutex_acquire(&mutex_robotStarted, TM_INFINITE);
         rs = robotStarted;
         rt_mutex_release(&mutex_robotStarted);
